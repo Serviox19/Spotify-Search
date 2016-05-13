00fc21e777b1f1c90db82dbb8651538adae846ae
Servio Mora <servio.mora19@gmail.com>
2016-05-13 19:49:32 -0400

    minor styling, added radio buttons for enhanced search, and able the search by pressing enter

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 var app = angular.module('spotifyApp', []);
 
 app.controller('mainController', function($scope) {
-
+  $scope.loadResults = function(event) {
+    event.preventDefault();
+    console.log('searched');
+  };
 })
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/bower_components', express.static(__dirname + "/bower_components"));
 
 
 app.get('*', function(req, res) {
-  res.sendFile(process.cwd() + '/public/index.html');
+  res.sendFile(process.cwd() + '/public/views/index.html');
 });
 
 

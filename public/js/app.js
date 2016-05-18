9a205de997d0be28f466bdc9101b40dd7c6d9c23
Servio Mora <servio.mora19@gmail.com>
2016-05-18 19:04:53 -0400

    cant get http response

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,14 @@
 var app = angular.module('spotifyApp', []);
 
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $http) {
+
   $scope.loadResults = function(event) {
     event.preventDefault();
-    console.log('searched');
+    // console.log('searched');
+    $http.get('https://api.spotify.com/v1/albums/' + $scope.search).success(function(response) {
+      console.log(response);
+    });
   };
+
+
 })
